71ed7647054c6d0ea2bd8ecbd6ae36a89597a339
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2015-12-23 21:31:28 -0300

    replacing the json dummy data with an Angular service using

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,42 +1,15 @@
-eventsApp.factory('eventData', function(){
+eventsApp.factory('eventData', function($http,$log){
 	
 	return {
-			event: {
-			name: 'Angular Boot Camp',
-			date: '1/1/2013',
-			time: '10:30 am',
-			location: {
-				address: 'Google Headquarters',
-				city: 'Mountain View',
-				province: 'CA'
-			},
-			imageUrl: '/img/angularjs-logo.png',
-			sessions:[
-				{
-					name: 'Directives Masterclass',
-					creatorName: 'Bob Smith',
-					duration: 1,
-					level: 'Advanced',
-					abstract: 'In this session you will learn the and outs of directives',
-					upVoteCount: 0
-				},
-				{
-					name: 'Scopes for fun and profit',
-					creatorName: 'JOhnDoe',
-					duration: 2,
-					level: 'Introductory',
-					abstract: 'In this session you will learn the and outs of directives',
-					upVoteCount: 0
-				},
-				{
-					name: 'Well Behaved Controllers',
-					creatorName: 'Jane Smith',
-					duration: 4,
-					level: 'Intermediate',
-					abstract: 'In this session you will learn the and outs of directives',
-					upVoteCount: 0
-				}
-			]
-		}
-	};
+			getEvent: function (successcallback) {
+				$http({method: 'GET', url: '/data/event/1'}).
+					success(function (data,status,headers,config) {
+						successcallback(data);
+					}).
+					error(function (data,status,headers,config){
+						$log.warn(data,status,headers(),config);
+
+					});
+			}
+		};
 });
