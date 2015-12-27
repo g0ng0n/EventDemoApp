9c2d986761246c524cd83eb79880e7d920ff8ef7
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2015-12-26 21:42:53 -0300

    adding the first part of the routing functionality

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-var eventsApp = angular.module('eventsApp', ['ngResource']);
+var eventsApp = angular.module('eventsApp', ['ngResource','ngRoute'])
+	.config(function($routeProvider){
+		$routeProvider.when('/newEvent',
+		{
+			templateUrl:'templates/NewEvent.html',
+			controller: 'EditEventController'
+		})
+});
+
